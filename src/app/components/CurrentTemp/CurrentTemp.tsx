7eb1d9d6c865e664d7728fe3a3d9fa1d4b72322f
fgrullon <franfelix@gmail.com>
2023-05-24 18:57:26 -0400

    add weather icon

--- src/app/components/CurrentTemp/CurrentTemp.tsx
+++ src/app/components/CurrentTemp/CurrentTemp.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { WiCelsius } from 'weather-icons-react';
+import { WiCelsius, WiDaySunny } from 'weather-icons-react';
 
 interface Props {
     city : String;
@@ -8,17 +8,18 @@ interface Props {
 const SearchBar = ({ city } : Props) => {
 
     return (
+        
        <div>
-        <h2>Madrid</h2>
-        <p>Chance of rain: 0%</p>
+            <h2>Madrid</h2>
+            <p>Chance of rain: 0%</p>
 
-        <div>
-            <h1>31</h1>
-        </div>
+            <div>
+                <h1>31 <WiCelsius size={25} color='#000' /></h1>
+            </div>
 
-        <div>
-            <h1>31 <WiCelsius size={25} color='#000' /></h1>
-        </div>
+            <div>
+                <h1><WiDaySunny size={200} color='yellow' /></h1>
+            </div>
 
        </div>
     );
